87ea283368e1e3602382e58ee5244366b8508d82
lenoxalmonacid <lalmonacid@belatrixsf.com>
2019-06-12 12:25:14 -0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NoAuthComponent, NotFoundComponent } from './alternative/components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadChildren: './main-component/modules/main.module#MainModule'
+  },
+  {
+    path: 'no-auth',
+    component: NoAuthComponent
+  },
+  { 
+    path: '**', 
+    redirectTo: '/not-found' 
+  },
+  { 
+    path: 'not-found', 
+    component: NotFoundComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
